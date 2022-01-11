7a27c1d83c533e8974cee0287102f81555d3c06d
Yoshihiro <87298805+yoshihiro0323@users.noreply.github.com>
2022-01-11 17:25:44 +0900

    Update ContentView.swift

--- Food Cash Register/Food Cash Register/ContentView.swift
+++ Food Cash Register/Food Cash Register/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView: View {
                             ScanView(orders: $inCart).tag(true)
                         }
                         .tabViewStyle(.page)
+                        .ignoresSafeArea()
                     }
                     }
                     .toolbar{
